10f43eb39da2da37c0d65bb1ecf6ce1f2932345d
xdjeremy <jeremy.fran@icloud.com>
2025-03-01 17:30:23 +0800

    Add development environment configuration and update OpenNext settings

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,7 +1,9 @@
 // default open-next.config.ts file created by @opennextjs/cloudflare
-import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
-import kvIncrementalCache from "@opennextjs/cloudflare/kv-cache";
+import { defineCloudflareConfig } from '@opennextjs/cloudflare/config';
+import kvIncrementalCache from '@opennextjs/cloudflare/kv-cache';
+import memoryQueue from '@opennextjs/cloudflare/memory-queue';
 
 export default defineCloudflareConfig({
-  incrementalCache: kvIncrementalCache,
+	incrementalCache: kvIncrementalCache,
+	queue: memoryQueue,
 });
