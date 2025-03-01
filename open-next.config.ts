96de5cfb04e1c8178ac6a088ae91e602827ed5b3
xdjeremy <jeremy.fran@icloud.com>
2025-03-02 02:56:55 +0800

    Remove development configuration files and clean up project structure

--- open-next.config.ts
+++
@@ -1,9 +0,0 @@
-// default open-next.config.ts file created by @opennextjs/cloudflare
-import { defineCloudflareConfig } from '@opennextjs/cloudflare/config';
-import kvIncrementalCache from '@opennextjs/cloudflare/kv-cache';
-import memoryQueue from '@opennextjs/cloudflare/memory-queue';
-
-export default defineCloudflareConfig({
-	incrementalCache: kvIncrementalCache,
-	queue: memoryQueue,
-});
