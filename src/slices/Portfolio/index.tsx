ae63a706a55891271cf0f00f4481bb72972f43f5
tamrivera05 <tamangelarivera@gmail.com>
2025-03-04 22:32:10 +0800

    Fix: Font weight on Portfolio page and a few fixes on the floaters

--- src/slices/Portfolio/index.tsx
+++ src/slices/Portfolio/index.tsx
@@ -2,27 +2,26 @@ import { FC } from 'react';
 import { Content } from '@prismicio/client';
 import { JSXMapSerializer, SliceComponentProps } from '@prismicio/react';
 import Badge from '@/components/common/Badge';
-import { Heading } from '@/components/Heading';
 import { PrismicRichText } from '@/components/PrismicRichText';
 import { PrismicNextImage } from '@prismicio/next';
-import { HalfBlueWaves2, OutlineSpark3, ThreeCircles } from '@/constants/icons';
+import {HalfBlueWaves2, OutlineSpark3, ThreeCircles, YellowWaves3} from '@/constants/icons';
 import { Bounded } from '@/components/Bounded';
 
 const component: JSXMapSerializer = {
-	heading2: ({ children }) => (
-		<Heading as='h2' size='lg'>
+	heading2: ({children}) => (
+		<h2 className='mb-5 mt-5 text-5xl font-extrabold md:mb-6 md:text-7xl lg:text-8xl'>
 			{children}
-		</Heading>
+		</h2>
 	),
-	heading3: ({ children }) => (
-		<Heading
-			as='h3'
-			size='xs'
-			className='mb-2 text-xl font-extrabold md:text-2xl'>
+	heading3: ({children}) => (
+		<h3 className='font-extrabold mb-2 text-xl font-bold md:text-2xl'>
 			{children}
-		</Heading>
+		</h3>
 	),
-	paragraph: ({ children }) => <p className='md:text-md'>{children}</p>,
+	strong: ({children}) => (
+		<span>{children}</span>
+	),
+	paragraph: ({children}) => <p>{children}</p>,
 };
 
 /**
@@ -39,11 +38,11 @@ const Portfolio: FC<PortfolioProps> = ({ slice }) => {
 	return (
 		<Bounded
 			as='section'
-			className='font-redhat text-brand-dark-blue relative px-[5%] py-16 md:py-24 lg:py-28 border-b-4 border-black'>
+			className='text-brand-dark-blue relative px-[5%] py-28 md:py-24 lg:py-28 border-b-4 border-black'>
 			<header className='mx-auto mb-12 max-w-lg text-center md:mb-18 lg:mb-20'>
 				<Badge variant='pink'>{badge}</Badge>
-				<PrismicRichText field={title} components={component} />
-				<PrismicRichText field={description} components={component} />
+				<PrismicRichText field={title} components={component}/>
+				<PrismicRichText field={description} components={component}/>
 			</header>
 			<div className='columns-1 after:block md:columns-2 md:gap-x-8 lg:columns-3'>
 				<article className='mb-8 break-inside-avoid'>
@@ -55,9 +54,10 @@ const Portfolio: FC<PortfolioProps> = ({ slice }) => {
 								boxShadow: `-15px 15px 0px 0px ${project[0]?.shadow_color}`,
 							}}
 						/>
-						<OutlineSpark3 className='w-[71px]  h-[71px] absolute -top-10 -left-4 md:-top-14 lg:w-[114px] lg:h-[114px] lg:-left-14 lg:-top-20' />
+						<OutlineSpark3
+							className='w-[71px]  h-[71px] absolute -top-10 -left-4 md:-top-14 lg:w-[114px] lg:h-[114px] lg:-left-14 lg:-top-20'/>
 					</div>
-					<PrismicRichText field={project[0]?.text} components={component} />
+					<PrismicRichText field={project[0]?.text} components={component}/>
 					<ul className='mt-3 flex flex-wrap gap-2 md:mt-4'>
 						<li className='flex'>
 							<div className='bg-background-secondary px-2 py-1 text-sm font-semibold'>
@@ -86,7 +86,7 @@ const Portfolio: FC<PortfolioProps> = ({ slice }) => {
 							}}
 						/>
 					</div>
-					<PrismicRichText field={project[1]?.text} components={component} />
+					<PrismicRichText field={project[1]?.text} components={component}/>
 					<ul className='mt-3 flex flex-wrap gap-2 md:mt-4'>
 						<li className='flex'>
 							<div className='bg-background-secondary px-2 py-1 text-sm font-semibold'>
@@ -120,7 +120,7 @@ const Portfolio: FC<PortfolioProps> = ({ slice }) => {
 							}}
 						/>
 					</div>
-					<PrismicRichText field={project[2]?.text} components={component} />
+					<PrismicRichText field={project[2]?.text} components={component}/>
 					<ul className='mt-3 flex flex-wrap gap-2 md:mt-4'>
 						<li className='flex'>
 							<div className='bg-background-secondary px-2 py-1 text-sm font-semibold'>
@@ -149,7 +149,7 @@ const Portfolio: FC<PortfolioProps> = ({ slice }) => {
 							}}
 						/>
 					</div>
-					<PrismicRichText field={project[3]?.text} components={component} />
+					<PrismicRichText field={project[3]?.text} components={component}/>
 					<ul className='mt-3 flex flex-wrap gap-2 md:mt-4'>
 						<li className='flex'>
 							<a
@@ -184,7 +184,7 @@ const Portfolio: FC<PortfolioProps> = ({ slice }) => {
 							}}
 						/>
 					</div>
-					<PrismicRichText field={project[4]?.text} components={component} />
+					<PrismicRichText field={project[4]?.text} components={component}/>
 					<ul className='mt-3 flex flex-wrap gap-2 md:mt-4'>
 						<li className='flex'>
 							<div className='bg-background-secondary px-2 py-1 text-sm font-semibold'>
@@ -214,10 +214,10 @@ const Portfolio: FC<PortfolioProps> = ({ slice }) => {
 						/>
 						<ThreeCircles
 							name='ThreeCircles'
-							className='w-[100px]  h-[280px] absolute bottom-[34%] -right-4 -z-10 md:-right-8 md:-bottom-32 lg:h-[336px] lg:bottom-[40%] lg:-right-12 lg:z-10'
+							className='w-[100px]  h-[280px] absolute bottom-[34%] -right-4 -z-10 md:-right-8 md:-bottom-32 lg:h-[336px] lg:bottom-[2%] lg:-right-12 lg:z-10'
 						/>
 					</div>
-					<PrismicRichText field={project[5]?.text} components={component} />
+					<PrismicRichText field={project[5]?.text} components={component}/>
 					<ul className='mt-3 flex flex-wrap gap-2 md:mt-4'>
 						<li className='flex'>
 							<div className='bg-background-secondary px-2 py-1 text-sm font-semibold'>
@@ -238,15 +238,23 @@ const Portfolio: FC<PortfolioProps> = ({ slice }) => {
 				</article>
 			</div>
 			<footer className='mt-8 flex justify-center md:mt-18 lg:mt-20 relative'>
-				<OutlineSpark3 className='w-[52px]  h-[52px] absolute right-32 bottom-10 -z-10 lg:w-[82px] lg:h-[82px] lg:right-1/4 ' />
+				<OutlineSpark3
+					className='w-[52px]  h-[52px] absolute right-32 bottom-10 -z-10 lg:w-[82px] lg:h-[82px] lg:right-1/4 '/>
 			</footer>
-			<HalfBlueWaves2 className='w-[182px]  h-[100px] absolute -top-0 right-8 -z-10 lg:w-[250px] lg:h-[180px] lg:right-28' />
-			<OutlineSpark3 className='w-[35px]  h-[35px] absolute top-16 left-20 lg:w-[62px] lg:h-[62px] lg:left-1/3' />
-			<OutlineSpark3 className='w-[48px]  h-[48px] absolute top-1/3 right-2 lg:w-[62px] lg:h-[62px] lg:top-1/4 lg:right-6' />
-			<OutlineSpark3 className='w-[42px]  h-[42px] absolute bottom-20 left-6 lg:w-[62px] lg:h-[62px] lg:bottom-32 lg:left-36' />
-			<OutlineSpark3 className='w-[160px] h-[120px] absolute bottom-0 -z-10 lg:w-[230px] lg:h-[190px] lg:left-40' />
-			<OutlineSpark3 className='w-[85px]  h-[85px] absolute right-4 bottom-2 lg:w-[139px] lg:h-[139px] lg:right-2' />
-			<OutlineSpark3 className='w-[49px]  h-[49px] absolute top-[70%] -left-4 lg:w-[75px] lg:h-[75px]' />
+			<HalfBlueWaves2
+				className='w-[182px]  h-[100px] absolute top-15 right-8 -z-10 lg:w-[250px] lg:h-[180px] lg:right-28'/>
+			<OutlineSpark3 className='w-[35px]  h-[35px] absolute top-24 left-16 lg:w-[62px] lg:h-[62px] lg:left-1/3'/>
+			<OutlineSpark3
+				className='w-[48px]  h-[48px] absolute top-1/3 right-2 lg:w-[62px] lg:h-[62px] lg:top-1/4 lg:right-6'/>
+			<OutlineSpark3
+				className='w-[42px]  h-[42px] absolute bottom-20 left-6 lg:w-[62px] lg:h-[62px] lg:bottom-32 lg:left-36'/>
+			<OutlineSpark3
+				className='w-[160px] h-[120px] absolute bottom-0 -z-10 lg:w-[230px] lg:h-[190px] lg:left-40'/>
+			<OutlineSpark3
+				className='w-[85px]  h-[85px] absolute right-4 bottom-2 lg:w-[139px] lg:h-[139px] lg:right-2'/>
+			<OutlineSpark3 className='w-[49px]  h-[49px] absolute top-[70%] -left-4 lg:w-[75px] lg:h-[75px]'/>
+			<YellowWaves3
+				className='w-[160px] h-[120px] absolute -bottom-24 -z-10 lg:w-[230px] lg:h-[190px] lg:left-40'/>
 		</Bounded>
 	);
 };
