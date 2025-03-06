8d13d769a06e3cacbdf9379a271986cc213b194d
xdjeremy <jeremy.fran@icloud.com>
2025-03-07 01:23:31 +0800

    Feat: Refactor ProcessStep component for improved consistency and styling

--- src/slices/ProcessOverview/components/ProcessStep.tsx
+++ src/slices/ProcessOverview/components/ProcessStep.tsx
@@ -1,19 +1,19 @@
-import { JSXMapSerializer } from '@prismicio/react';
-import { FC } from 'react';
 import { Heading } from '@/components/Heading';
 import { PrismicRichText } from '@/components/PrismicRichText';
-import { Lightbulb, Pencil, Box } from 'lucide-react';
+import { JSXMapSerializer } from '@prismicio/react';
+import { Box, Lightbulb, Pencil } from 'lucide-react';
+import { FC } from 'react';
 
 const component: JSXMapSerializer = {
   heading3: ({ children }) => (
-    <Heading as='h3' size='xs'>
+    <Heading as="h3" size="xs">
       {children}
     </Heading>
   ),
   strong: ({ children }) => (
-    <span className='text-brand-telemagenta'>{children}</span>
+    <span className="text-brand-telemagenta">{children}</span>
   ),
-  paragraph: ({ children }) => <p>{children}</p>,
+  paragraph: ({ children }) => <p>{children}</p>
 };
 
 import { Content } from '@prismicio/client';
@@ -26,16 +26,16 @@ interface ProcessStepProps {
 }
 
 const iconComponent = (icon: ProcessStepProps['icon']) => {
-  if (!icon) return <Lightbulb className='size-12' />;
+  if (!icon) return <Lightbulb className="size-12" />;
   switch (icon) {
     case 'lightbulb':
-      return <Lightbulb className='size-12' />;
+      return <Lightbulb className="size-12" />;
     case 'pencil':
-      return <Pencil className='size-12' />;
+      return <Pencil className="size-12" />;
     case 'box':
-      return <Box className='size-12' />;
+      return <Box className="size-12" />;
     default:
-      return <Lightbulb className='size-12' />;
+      return <Lightbulb className="size-12" />;
   }
 };
 
@@ -43,11 +43,11 @@ export const ProcessStep: FC<ProcessStepProps> = ({
   icon,
   title,
   description,
-  decorations,
+  decorations
 }) => {
   return (
-    <div className='neobrutalist-shadow relative flex h-full flex-col items-center px-6 py-4 text-center'>
-      <div className='mb-5 md:mb-6'>{iconComponent(icon)}</div>
+    <div className="neobrutalist-shadow relative flex h-full flex-col items-center px-6 py-4 text-center bg-white">
+      <div className="mb-5 md:mb-6">{iconComponent(icon)}</div>
       <PrismicRichText field={title} components={component} />
       <PrismicRichText field={description} components={component} />
       {decorations}
