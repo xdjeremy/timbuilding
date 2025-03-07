121abdfd7b9ae66d707b55900f1b28e01fc5f1f9
xdjeremy <jeremy.fran@icloud.com>
2025-03-07 23:31:24 +0800

    Refactor: Replace section with Bounded component in Hero for improved layout structure

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,17 +1,16 @@
 'use server';
 
+import { Bounded } from '@/components/Bounded';
+import Button from '@/components/common/Button';
+import { PrismicRichText } from '@/components/PrismicRichText';
+import { BlueStar2, PinkStar } from '@/constants/icons';
+import { HeroDecorations } from '@/slices/Hero/HeroDecorations';
+import { HeroImageGrid } from '@/slices/Hero/HeroImageGrid';
+import { components } from '@/slices/Hero/HeroRichText';
 import { type Content } from '@prismicio/client';
 import { PrismicNextLink } from '@prismicio/next';
 import { type SliceComponentProps } from '@prismicio/react';
 import { FC } from 'react';
-
-import Button from '@/components/common/Button';
-import { PrismicRichText } from '@/components/PrismicRichText';
-import { components } from '@/slices/Hero/HeroRichText';
-
-import { BlueStar2, PinkStar } from '@/constants/icons';
-import { HeroDecorations } from '@/slices/Hero/HeroDecorations';
-import { HeroImageGrid } from '@/slices/Hero/HeroImageGrid';
 
 type HeroProps = SliceComponentProps<Content.HeroSlice>;
 
@@ -19,7 +18,10 @@ const Hero: FC<HeroProps> = async ({ slice }) => {
   const { buttonLink, buttonText, title, subtitle, images } = slice.primary;
 
   return (
-    <section className="font-redhat text-brand-dark-blue grid grid-cols-1 gap-y-16 border-b-4 border-black pt-16 md:grid-flow-row md:pt-24 lg:grid-flow-col lg:grid-cols-2 lg:items-center lg:pt-0">
+    <Bounded
+      as="section"
+      className="grid grid-cols-1 gap-y-16 px-0 pb-0 md:grid-flow-row md:pb-0 lg:grid-flow-col lg:grid-cols-2 lg:items-center lg:py-0 lg:pt-0"
+    >
       <div className="mx-[5%] max-w-[40rem] justify-self-start lg:mr-20 lg:ml-[5vw] lg:justify-self-end">
         <div className="lg:text-10xl mb-5 text-6xl font-extrabold md:mb-6 md:text-9xl">
           <PrismicRichText field={title} components={components} />
@@ -38,7 +40,7 @@ const Hero: FC<HeroProps> = async ({ slice }) => {
         <HeroImageGrid images={images} />
         <HeroDecorations />
       </div>
-    </section>
+    </Bounded>
   );
 };
 
